53feab7344b3e8abd586130ac72cc705f9410176
wholteza <zackarias@montell.se>
2025-01-02 21:00:39 +0000

    wip some menu

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,17 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.scss";
-
+import "./main.scss"
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
+    <div className="container">
+      <div className="menu">
+        menu
+      </div>
+      <div className="content">
+        
     <App />
+    </div>
+    </div>
   </StrictMode>
 );
