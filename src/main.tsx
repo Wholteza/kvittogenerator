1fcab4096f3495293d12fe199394f5623985c878
wholteza <zackarias@montell.se>
2025-01-04 18:41:37 +0000

    Add logotype, started with routing

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,20 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
+import {  } from "react-router-dom";
+
 import "./index.scss";
 import "./main.scss"
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
     <div className="container">
       <div className="menu">
-        menu
+        <img src="/logotype.webp"></img>
       </div>
       <div className="content">
+        <Switch>
+          
+        </Switch>
         
     <App />
     </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
+import { reactRouter } from "@react-router/dev/vite"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), reactRouter()],
 })
