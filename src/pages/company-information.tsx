7208247500f568cd795c069f965e76b808ccf1f7
wholteza <zackarias@montell.se>
2025-01-06 17:14:45 +0100

    Removed unused import

--- src/pages/company-information.tsx
+++ src/pages/company-information.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext } from "react";
 import TextInput from "~components/text-input";
 import Typography from "~components/typography";
 import { type CompanyInformation } from "~types";
